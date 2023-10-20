ee87ce715afead8211204e26397fc0ce673480ea
devkkyu <dev.kkyu@gmail.com>
2023-10-21 01:22:53 +0900

    수풀 수정 및 배치

--- ArcaneSoul/background.py
+++ ArcaneSoul/background.py
@@ -1,6 +1,6 @@
 from pico2d import *
 
-class Grass:
+class Background:
     def __init__(self):
         self.image = load_image('Resources/Background/b1.png')
 
@@ -12,3 +12,13 @@ class Grass:
                 self.image.draw(x + 249, 450, 498, 900)
 
 
+class Grass:
+    def __init__(self):
+        self.image = load_image('Resources/Background/n.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        for x in range(0, 1600, 68):
+                self.image.draw(x + 34, 83)
